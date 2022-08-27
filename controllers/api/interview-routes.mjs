24e1c36b2122e95e048ca88cdff2e1b07925fee3
Gospel Chukwu <brondchux@gmail.com>
2022-08-27 15:45:25 -0400

    Protected all interviews routes using protect middleware

--- controllers/api/interview-routes.mjs
+++ controllers/api/interview-routes.mjs
@@ -12,50 +12,87 @@ router.get("/", protect, async (req, res) => {
 			where: { id: req.user.id },
 			include: { model: User, attributes: { exclude: ["password"] } },
 		});
-		res.json({ status: 200, error: false, data: interviews });
+		res.status(200).json({
+			status: res.statusCode,
+			error: false,
+			data: interviews,
+			message: "Found user interviews.",
+		});
 	} catch (err) {
-		res.json({ status: 400, error: err, data: null });
+		res.status(400).json({
+			status: res.statusCode,
+			error: true,
+			data: null,
+			message: "Fetching user interviews failed!",
+		});
 	}
 });
 
 // GET /api/interviews/:id
-router.get("/:id", async (req, res) => {
+router.get("/:id", protect, async (req, res) => {
 	const id = xss(req.params.id);
 	try {
 		const interview = await Interview.findOne({
-			where: { id },
-			include: { model: User, attributes: { exclude: ["password"] } },
+			where: { id, user_id: req.user.id },
 		});
-		res.json({ status: 200, error: false, data: interview });
+		res.status(200).json({
+			status: res.statusCode,
+			error: false,
+			data: interview,
+			message: "Found user interview.",
+		});
 	} catch (err) {
-		res.json({ status: 400, error: err, data: null });
+		res.status(400).json({
+			status: res.statusCode,
+			error: true,
+			data: null,
+			message: "Fetching user interview failed!",
+		});
 	}
 });
 
-// PUT /api/interviews/:userId/:id
-router.put("/:userId/:id", async (req, res) => {
-	const userId = xss(req.params.userId);
+// PUT /api/interviews/:id
+router.put("/:id", protect, async (req, res) => {
 	const id = xss(req.params.id);
 	try {
 		const interview = await Interview.update(
 			{ active: false },
-			{ where: { id, user_id: userId }, returning: true, plain: true }
+			{ where: { id, user_id: req.user.id }, returning: true, plain: true }
 		);
-		res.json({ status: 200, error: false, data: interview });
+		res.status(200).json({
+			status: res.statusCode,
+			error: false,
+			data: interview,
+			message: "Updated user interview.",
+		});
 	} catch (err) {
-		res.json({ status: 400, error: err, data: null });
+		res.status(400).json({
+			status: res.statusCode,
+			error: true,
+			data: null,
+			message: "Updating user interview failed!",
+		});
 	}
 });
 
-// DELETE /api/interviews/:userId/:id
-router.delete("/:userId/:id", async (req, res) => {
-	const userId = xss(req.params.userId);
+// DELETE /api/interviews/:id
+router.delete("/:id", protect, async (req, res) => {
 	const id = xss(req.params.id);
 	try {
-		await Interview.destroy({ where: { id, user_id: userId } });
-		res.json({ status: 200, error: false, data: { destroyed: true } });
+		await Interview.destroy({ where: { id, user_id: req.user.id } });
+		res.status(200).json({
+			status: res.statusCode,
+			error: false,
+			data: null,
+			message: "Deleted user interview.",
+		});
 	} catch (err) {
-		res.json({ status: 400, error: err, data: null });
+		res.status(400).json({
+			status: res.statusCode,
+			error: true,
+			data: null,
+			message: "Deleting user interview failed!",
+		});
 	}
 });
 
