55fb90d64dfc3a3b8094a3187a3b3509d2157265
Gospel Chukwu <brondchux@gmail.com>
2022-10-10 18:20:14 -0400

    Commented out connection import - TS

--- app.mjs
+++ app.mjs
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
 // import routes from "./controllers";
-import db from "./config/connection";
+// import db from "./config/connection";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -22,10 +22,10 @@ app.get("/", (req, res) => {
 	});
 });
 
-// Sequelize setup
-db.authenticate()
-	.then(() => console.log("DB connection has been established successfully."))
-	.catch((err) => console.error("Unable to connect to the database:", err));
+// // Sequelize setup
+// db.authenticate()
+// 	.then(() => console.log("DB connection has been established successfully."))
+// 	.catch((err) => console.error("Unable to connect to the database:", err));
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
