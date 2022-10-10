0c9eb9debd7fc1ca60f061a8c4143184cbb5b78c
Gospel Chukwu <brondchux@gmail.com>
2022-10-10 16:50:14 -0400

    Added url for Postgres elephant SQL

--- config/connection.mjs
+++ config/connection.mjs
@@ -1,14 +1,16 @@
 import Sequelize from "sequelize";
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASS,
-	{
-		host: "localhost",
-		dialect: "postgres",
-		logging: false,
-	}
-);
+const sequelize = process.env.POSTGRES_URI
+	? new Sequelize(process.env.POSTGRES_URI, { logging: false })
+	: new Sequelize(
+			process.env.DB_NAME,
+			process.env.DB_USER,
+			process.env.DB_PASS,
+			{
+				host: "localhost",
+				dialect: "postgres",
+				logging: false,
+			}
+	  );
 
 export default sequelize;
