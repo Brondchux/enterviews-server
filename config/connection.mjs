e2f52bd4352d6649e1373bbdd32b5b1e21eb6f7a
Gospel Chukwu <brondchux@gmail.com>
2022-10-11 13:59:03 -0400

    Added DB connection details

--- config/connection.mjs
+++ config/connection.mjs
@@ -1,16 +1,27 @@
 import Sequelize from "sequelize";
 
-const sequelize = process.env.POSTGRES_URI
-	? new Sequelize(process.env.POSTGRES_URI, { logging: false })
-	: new Sequelize(
-			process.env.DB_NAME,
-			process.env.DB_USER,
-			process.env.DB_PASS,
-			{
-				host: "localhost",
-				dialect: "postgres",
-				logging: false,
-			}
-	  );
+const sequelize = new Sequelize(
+	process.env.DB_NAME,
+	process.env.DB_USER,
+	process.env.DB_PASS,
+	{
+		host: process.env.DB_HOST,
+		dialect: "postgres",
+		logging: false,
+	}
+);
+
+// const sequelize = process.env.POSTGRES_URI
+// 	? new Sequelize(process.env.POSTGRES_URI, { logging: false })
+// 	: new Sequelize(
+// 			process.env.DB_NAME,
+// 			process.env.DB_USER,
+// 			process.env.DB_PASS,
+// 			{
+// 				host: process.env.DB_HOST,
+// 				dialect: "postgres",
+// 				logging: false,
+// 			}
+// 	  );
 
 export default sequelize;
