8a408836ef5e94e4568e62a88d3931c33cf8d905
Gospel Chukwu <brondchux@gmail.com>
2022-10-11 15:05:45 -0400

    Moved json and urlencoded position

--- config/connection.mjs
+++ config/connection.mjs
@@ -11,17 +11,4 @@ const sequelize = new Sequelize(
 	}
 );
 
-// const sequelize = process.env.POSTGRES_URI
-// 	? new Sequelize(process.env.POSTGRES_URI, { logging: false })
-// 	: new Sequelize(
-// 			process.env.DB_NAME,
-// 			process.env.DB_USER,
-// 			process.env.DB_PASS,
-// 			{
-// 				host: process.env.DB_HOST,
-// 				dialect: "postgres",
-// 				logging: false,
-// 			}
-// 	  );
-
 export default sequelize;
