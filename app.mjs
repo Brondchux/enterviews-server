8946afc4074535e74624c7aa08b547d547abc66d
Gospel Chukwu <brondchux@gmail.com>
2022-10-12 19:13:50 -0400

    Removed test get-route

--- app.mjs
+++ app.mjs
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import express from "express";
-// import routes from "./controllers";
-// import db from "./config/connection";
+import routes from "./controllers";
+import db from "./config/connection";
 import cors from "cors";
 
 const app = express();
@@ -14,22 +14,13 @@ app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// tester
-app.get("/", (req, res) => {
-	res.status(200).json({
-		status: res.statusCode,
-		error: false,
-		data: { message: "Welcome to Enterviews RESTful APIs." },
-	});
-});
+// Routes setup
+app.use(routes);
 
-// // Routes setup
-// app.use(routes);
-
-// // Sequelize setup
-// db.authenticate()
-// 	.then(() => console.log("DB connection has been established successfully."))
-// 	.catch((err) => console.error("Unable to connect to the database:", err));
+// Sequelize setup
+db.authenticate()
+	.then(() => console.log("DB connection has been established successfully."))
+	.catch((err) => console.error("Unable to connect to the database:", err));
 
 app.listen(PORT, () => {
 	console.log(`Listening on port ${PORT}`);
